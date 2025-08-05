86435bed340336a89fa378eb86cedaf055f4bd5f
davy2333 <deivimauriciodavid@gmail.com>
2025-08-04 20:09:19 -0600

    avances

--- Scripts.js
+++ Scripts.js
@@ -1,8 +1,30 @@
-let pc = {
-    nombre: "davypc",
-    procesador: "IntelCore19",
-    memoria: "1tb",
-    ram: "16gb"
+class animal
+{
+    constructor(nombre, edad)
+    {
+        this.nombre = nombre;
+        this.edad = edad;
+    }
+
+    showInfo(){
+        document.write(`El nombre del animal es 
+            ${this.nombre} y tiene ${this.edad} 
+            años y su raza es ${this.raza}<br>`);
+    }
 }
 
-document.write(pc["nombre"])
+class Perro extends animal 
+{
+    constructor(nombre, edad, raza)
+    {
+        super(nombre, edad);
+        this.raza = null;
+    }
+    set modificarRaza (newName){
+        this.raza = newName;
+    }
+}
+const dog = new Perro( "Firulais", 5, "Beagle");
+
+dog.modificarRaza = "Labrador";
+document.write( dog.raza );
