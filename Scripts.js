77e47aff8636528d8078ac8357a02599af5daad3
davy2333 <deivimauriciodavid@gmail.com>
2025-08-05 21:52:45 -0600

    actu

--- Scripts.js
+++ Scripts.js
@@ -1,30 +1,3 @@
-class animal
-{
-    constructor(nombre, edad)
-    {
-        this.nombre = nombre;
-        this.edad = edad;
-    }
-
-    showInfo(){
-        document.write(`El nombre del animal es 
-            ${this.nombre} y tiene ${this.edad} 
-            años y su raza es ${this.raza}<br>`);
-    }
+class telefono{
+    
 }
-
-class Perro extends animal 
-{
-    constructor(nombre, edad, raza)
-    {
-        super(nombre, edad);
-        this.raza = null;
-    }
-    set modificarRaza (newName){
-        this.raza = newName;
-    }
-}
-const dog = new Perro( "Firulais", 5, "Beagle");
-
-dog.modificarRaza = "Labrador";
-document.write( dog.raza );
